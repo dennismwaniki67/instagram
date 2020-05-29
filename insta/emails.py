b6670c58b9b76f4c2bcd34a77cb8928a4b8a858a
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-29 19:58:46 +0300

    created a send_welcome_email function

--- insta/emails.py
+++ insta/emails.py
@@ -2,8 +2,8 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
-    subject = 'Welcome to InstaPic'
-    sender = 'ijanemercy@gmail.com'
+    subject = 'Welcome to Instagram'
+    sender = 'dennismwaniki67@gmail.com'
 
     text_content = render_to_string('emails/email.txt',{"name":name})
     html_content = render_to_string('emails/email.html',{"name":name})
